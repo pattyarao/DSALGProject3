2c47ddcc71e5f16c15463accb2c438af66b66c5a
Patty Arao <110594488+pattyarao@users.noreply.github.com>
2023-04-06 21:11:45 +0800

    Add files via upload

---
+++ BST.java
@@ -0,0 +1,50 @@
+package project3;
+
+public class BST {
+
+	private Node rootNode;
+	
+	public BST() {
+		//TODO: your code here
+	}
+	
+	public void insert(int data) {
+		//TODO: your code here
+	}
+	
+	public Node search(int x) {
+		//TODO: your code here
+	}
+	
+	public void inOrder() {
+		//TODO: your code here
+	}
+	
+	public void preOrder() {
+		//TODO: your code here
+	}
+	
+	public void postOrder() {
+		//TODO: your code here
+	}
+	
+	public Node maximum() {
+		//TODO: your code here
+	}
+	
+	public Node minimum() {
+		//TODO: your code here
+	}
+	
+	public Node parent(int data) {
+		//TODO: your code here
+	}
+	
+	public Node successor(int data) {
+		//TODO: your code here
+	}
+	
+	public Node predecessor(int data) {
+		//TODO: your code here
+	}
+}
