1925806ebc83a550453d492ef36fbc072447de2b
Jerrick-Santos-DLSU <jerrickezekiel0122@gmail.com>
2023-04-07 15:29:10 +0800

    Implemented parent, successor, and predecessor functions

--- BST.java
+++ BST.java
@@ -67,15 +67,90 @@ public class BST {
 		// TODO: your code here
 	}
 
+
 	public Node parent(int data) {
 		// TODO: your code here
+		return parentSearch(this.rootNode,data);
 	}
+
+	public Node parentSearch(Node node, int data){
+		if (node.getData() == data || (node.getLeftNode() == null
+				&& node.getRightNode() == null)){
+			return null;
+		}
+
+		if (node.getLeftNode() != null && node.getLeftNode().getData() == data){
+			return node;
+		}
+		else if (node.getRightNode() != null && node.getRightNode().getData() == data){
+			return node;
+		}
+		else if (data < node.getData()) {
+			return parentSearch(node.getLeftNode(), data);
+		} else {
+			return parentSearch(node.getRightNode(), data);
+		}
+
+	}
+
 
 	public Node successor(int data) {
 		// TODO: your code here
+
+		Node temp = search(data);
+
+		if (temp.getRightNode() != null){
+			return minFinder(temp.getRightNode());
+		}
+
+		Node parentNode = parent(data);
+
+		while (parentNode != null &&
+				temp.getData() == parentNode.getRightNode().getData()
+				&& parentNode.getRightNode() != null){
+			temp = parentNode;
+			parentNode = parent(parentNode.getData());
+		}
+
+		return parentNode;
+	}
+
+	public Node minFinder(Node node){
+		Node temp = node;
+		while (temp.getLeftNode() != null){
+			temp = temp.getLeftNode();
+		}
+
+		return temp;
 	}
 
 	public Node predecessor(int data) {
 		// TODO: your code here
+
+		Node temp = search(data);
+
+		if (temp.getLeftNode() != null){
+			return maxFinder(temp.getLeftNode());
+		}
+
+		Node parentNode = parent(data);
+
+		while (parentNode != null
+				&& temp.getData() == parentNode.getLeftNode().getData()
+				&& parentNode.getLeftNode() != null){
+			temp = parentNode;
+			parentNode = parent(parentNode.getData());
+		}
+
+		return parentNode;
+	}
+
+	public Node maxFinder(Node node){
+		Node temp = node;
+		while (temp.getRightNode() != null){
+			temp = temp.getRightNode();
+		}
+
+		return temp;
 	}
 }
